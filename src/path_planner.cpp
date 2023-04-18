1bb80fa3eae031072dd68a0415383450866f66ed
pariaspe <pedro.ariasp@upm.es>
2023-04-18 11:11:18 +0200

    test

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -531,7 +531,7 @@ cv::Point2i PathPlanner::coord2img(const float _x, const float _y,
   float x = _x * img_resolution_;
   float y = _y * img_resolution_;
   img_point.x = int(x);
-  img_point.y = int(_img_h - y);
+  img_point.y = int(y);
 
   // saturate with _img_h and _img_w
 
